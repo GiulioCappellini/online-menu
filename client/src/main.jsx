16b6e79dae0ef8ec61502da111de1485be6723f1
Giulio Cappellini <dev.cappellini@gmail.com>
2026-07-15 15:42:01 +0200

    Inicializate and clear defawlt vite content

---
+++ client/src/main.jsx
@@ -0,0 +1,13 @@
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+
+import App from './App.jsx';
+
+// Import styles
+import './styles/reset.css';
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
