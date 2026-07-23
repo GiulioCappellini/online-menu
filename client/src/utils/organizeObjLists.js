f8edf245f71c47912adf842a8afb8cf52ea7981c
Giulio Cappellini <dev.cappellini@gmail.com>
2026-07-23 15:18:39 +0200

    Alternate the code to always place the english above the italiano, on the second position

--- client/src/utils/organizeObjLists.js
+++ client/src/utils/organizeObjLists.js
@@ -1,9 +1,15 @@
 function organizeList(languagesData) {
     return (
         [...languagesData].sort((a, b) => {
+            // Italian always first
             if (a.code === "it") return -1;
             if (b.code === "it") return 1;
 
+            // English always second
+            if (a.code === "en") return -1;
+            if (b.code === "en") return 1;
+
+            // The rest alphabetically
             return a.name.localeCompare(b.name);
         }).map((item, index) => ({
             ...item,
